86906dca31ea0532622ccf575cb50a9f8c5c73f7
Anmol Singh <klwndrsoni@gmail.com>
2025-05-24 19:09:14 +0530

    changed dcdfcvrgnbvrgbebetfdssbnssevfed

--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 // Define the Person schema
 console.log("hi");
+const bcrypt=require('bcrypt');
+
 
 const personSchema = new mongoose.Schema ({
 name: {
@@ -32,6 +34,42 @@ required: true,
 type: String}
 
 } ) ;
+
+personSchema. pre ( 'save', async function(next) {
+const person = this;
+// Hash the password only if it has been modified (or is new)
+if ( !person. isModified('password')) return next ();
+try{
+// hash password generation
+const salt = await bcrypt.genSalt(10);
+// hash password
+const hashedPassword = await bcrypt.hash(person.password, salt);
+
+// Override the plain password with the hashed one
+person. password = hashedPassword;
+next ();
+
+}catch (err) {
+return next(err)
+;
+}});
+
+personSchema. methods . comparePassword = async function (candidatePassword) {
+try{
+// Use bcrypt to compare the provided password with the hashed password
+const isMatch = await bcrypt. compare(candidatePassword, this.password) ;
+return isMatch;
+}catch (err) {
+throw err;}}
+
+// prince --- > bdbebuebjebe7y38eh3jbeu3heb3You
+// login --- > agarwak
+// bdbebuebjebe7y38eh3jbeu3heb3 -- > extract salt
+// salt+agarwak -- > hash -- > svwsnwkmskwmswbshwvdhwjdnbejdne 
+
+
+
+
 
 
 //making model 
